3c65015b136dfb5e36a6800bde81b647bfc27bb0
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-04 15:30:14 +0800

    >  compile NEMU ysyx_22040222 陈学军 Linux sdzz-VirtualBox 5.11.0-34-generic #36-Ubuntu SMP Thu Aug 26 19:22:09 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:30:14 up  3:16,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -9,6 +9,10 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  int i = 0;
+  for(i = 0; i < 32; i+=4){
+	  printf("%s\t%lu\t%s\t%lu\t%s\t%lu\t%s\t%lu\n",regs[i],cpu.gpr[i],regs[i+1],cpu.gpr[i+1],regs[i+2],cpu.gpr[i+2],regs[i+3],cpu.gpr[i+3]);
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
